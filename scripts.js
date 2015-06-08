b880050a605e827cd970487d093282a98aaa1389
slerpy <slerpy@riseup.net>
2015-06-08 02:49:32 -0700

    getting things reset and basic setup ready.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("Program Started Successfully.")
+
+
+
+
+var favoriteIssue = prompt("What issues are you most interested in?");
+document.write("issues: " + favoriteIssue.toLowerCase());
+
+
+
+console.log("Program Completed Successfully.");
