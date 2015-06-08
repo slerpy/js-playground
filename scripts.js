81b1883841fdf45cd8a745834cef6030e7a18492
slerpy <slerpy@riseup.net>
2015-06-08 03:28:01 -0700

    i suppose readability is groovy.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ adjective = prompt("Now an adjective, please.");
 adverb = prompt("K, now an adverb");
 phrase = prompt("K, lastly, a phrase such as 'omg! a hotdog!'")
 
-story = "A man " + adverb.toLowerCase() + " " + verb.toLowerCase() + " " + "to the " + noun.toLowerCase() + " and shouted " + phrase + "!"
+story = "<h2>A man " + adverb.toLowerCase() + " " + verb.toLowerCase() + " " + "to the " + noun.toLowerCase() + " and shouted " + phrase + "!</h2>";
 
 document.write(story);
 
