e9f6a4371cfeea5c61fdc15f03e2e741df7dc9b4
slerpy <slerpy@riseup.net>
2015-06-08 04:45:56 -0700

    forgot that I'm not coding in python...USE VAR OMG!

---
+++ scripts.js
@@ -0,0 +1,21 @@
+//
+// 04-06
+//
+
+var secondsPerMinute = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMinute * minsPerHour * hoursPerDay;
+
+console.log(secondsPerDay);
+
+var yearsAlive = 25;
+var secondsAlive = secondsPerDay * daysPerWeek * weeksPerYear * yearsAlive;
+
+console.log(secondsAlive);
+
+//
+// 04-07
+//
--- story.js
+++ story.js
@@ -1,12 +1,13 @@
 console.log("Program Started Successfully.")
 
-verb = prompt("Gimme A Verb.");
-noun = prompt("Gimme A Noun.");
-adjective = prompt("Now an adjective, please.");
-adverb = prompt("K, now an adverb");
-phrase = prompt("K, lastly, a phrase such as 'omg! a hotdog!'")
 
-story = "<h2>A man " + adverb.toLowerCase() + " " + verb.toLowerCase() + " " + "to the " + noun.toLowerCase() + " and shouted " + phrase + "!</h2>";
+var verb = prompt("Gimme A Verb.");
+var noun = prompt("Gimme A Noun.");
+var adjective = prompt("Now an adjective, please.");
+var adverb = prompt("K, now an adverb");
+var phrase = prompt("K, lastly, a phrase such as 'omg! a hotdog!'")
+
+var story = "<h2>A man " + adverb.toLowerCase() + " " + verb.toLowerCase() + " " + "to the " + noun.toLowerCase() + " and shouted " + phrase + "!</h2>";
 
 document.write(story);
 
