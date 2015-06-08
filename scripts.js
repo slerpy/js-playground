993851de30d41785a8d0ca1d8d8b2221f50c7d6c
slerpy <slerpy@riseup.net>
2015-06-08 03:21:23 -0700

    rough - really rough - sketch of the basic idea.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
 console.log("Program Started Successfully.")
 
+verb = prompt("Gimme A Verb.");
+noun = prompt("Gimme A Noun.");
+adjective = prompt("Now an adjective, please.");
+adverb = prompt("K, now an adverb");
+phrase = prompt("K, lastly, a phrase such as 'omg! a hotdog!'")
 
+story = "A man " + adverb.toLowerCase() + " " + verb.toLowerCase() + " " + "to the " + noun.toLowerCase() + " and shouted " + phrase + "!"
 
+document.write(story);
 
-var favoriteIssue = prompt("What issues are you most interested in?");
-document.write("issues: " + favoriteIssue.toLowerCase());
+console.log(verb);
+console.log(noun);
+console.log(adjective);
+console.log(adverb);
+console.log(phrase);
+
 
 
 
