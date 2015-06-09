704255039131ef0426ddf8a7733cc87889b304cd
slerpy <slerpy@riseup.net>
2015-06-08 21:15:00 -0700

    random experimentation in js.

--- luckyno.js
+++ luckyno.js
@@ -2,4 +2,4 @@ var userNumber = prompt("To find your lucky number, give me an integer between 1
 var random = Math.floor(Math.random() * parseInt(userNumber)) + 1;
 //console.log(random);
 document.write("<h2>OOOH! Your lucky number is " + random + "!</h2>");
-document.write("<h4>My patent pending algorithm is really good at picking lucky numbers. Go play the lottery or something.</h4>")
+document.write("<h4>My patent pending algorithm is really good at picking lucky numbers. Go play the lottery or something.</h4>");
--- scripts.js
+++ scripts.js
@@ -20,3 +20,14 @@ console.log(secondsAlive);
 // 04-07
 //
 
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+
+var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+
+if (parseInt(guess) === randomNumber ) {
+    document.write('<p>Sorcerer!</p>');
+} else {
+    document.write('<p> Nopers. It was ' + randomNumber + '</p>');
+}
+
