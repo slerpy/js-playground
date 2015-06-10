b715e1ef726e49ade07886efb921d54c00ec1fe3
slerpy <slerpy@riseup.net>
2015-06-10 01:28:55 -0700

    Reset for next project.

--- luckyno.js
+++
@@ -1,5 +0,0 @@
-var userNumber = prompt("To find your lucky number, give me an integer between 1 and 100.");
-var random = Math.floor(Math.random() * parseInt(userNumber)) + 1;
-//console.log(random);
-document.write("<h2>OOOH! Your lucky number is " + random + "!</h2>");
-document.write("<h4>My patent pending algorithm is really good at picking lucky numbers. Go play the lottery or something.</h4>");
