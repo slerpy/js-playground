95c7fea79fd5d6936db78037b191c810b5a39af6
slerpy <slerpy@riseup.net>
2015-06-10 04:55:51 -0700

    tweaking

--- stronk/stronk.js
+++ stronk/stronk.js
@@ -5,7 +5,7 @@
     generated number.
  */
 
-var upper = 100000000;
+var upper = 10000000;
 var randomNumber = getRandomNumber(upper);
 var guess;
 var attempts = 0;
@@ -13,11 +13,12 @@ var attempts = 0;
 function getRandomNumber (upper) {
     return Math.floor( Math.random() * upper ) + 1;
 }
-
+var tStart = performance.now();
 while ( guess !== randomNumber ) {
     guess = getRandomNumber(upper);
     attempts += 1;
 }
-
+var tEnd = performance.now();
 document.write('Our number is: <strong>' + randomNumber + '</strong><br><br>');
-document.write('It took the computer <strong>' + attempts + '</strong> guesses to get our number.');
+document.write('It took the computer <strong>' + attempts + '</strong> guesses to get our number.<br><br>');
+document.write('<h6>It took <strong>' + Math.floor(tEnd - tStart) + '</strong> milliseconds to guess <strong>' + attempts.toLocaleString() + '</strong> times.</h6>')
