bf2f51c8d5ccc62a556bdd30e5c4fb6acc91e79f
slerpy <slerpy@riseup.net>
2015-06-13 02:25:56 -0700

    search that array foo!

---
+++ js/basicArrayListSearching.js
@@ -0,0 +1,30 @@
+/*
+    simple web page search to see if something is 'in stock.'
+    basically an array search lol.
+ */
+
+
+
+var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza',
+    'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var search;
+
+function print( message ) {
+    document.write( '<p>' + message + '</p>');
+}
+
+while (true) {
+    search = prompt("Search for a product. Type 'list' to show what is in stock, and type 'quit' to exit");
+    search = search.toLowerCase();
+    if (search === 'quit') {
+        break;
+    } else if ( search === 'list' ){
+        print(inStock.join(', '));
+    } else {
+        if ( inStock.indexOf( search ) > -1 ) {
+            print('Yes, we have ' + search + ' in the store.');
+        } else {
+            print(search + ' is not in stock.');
+        }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -72,30 +72,46 @@
     Refactor above into  a simple loop.
  */
 
-var html = '';
-//var randomRGBColor;
+//var html = '';
+////var randomRGBColor;
+//
+//function letsGetRandom() {
+//    return Math.floor(Math.random() * 256 );
+//}
+//
+//function randomColor() {
+//    var color = 'rgb(';
+//    color += letsGetRandom() + ',';
+//    color += letsGetRandom() + ',';
+//    color += letsGetRandom() + ')';
+//    return color;
+//}
+//
+//function print(message) {
+//    document.write(message);
+//}
+//function ooohColors () {
+//    html += '<div style="background-color:' + randomColor() + '"></div>';
+//}
+//
+//for (i = 0; i < 10; i += 1) {
+//    ooohColors();
+//}
+//
+//print(html);
 
-function letsGetRandom() {
-    return Math.floor(Math.random() * 256 );
+
+var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza',
+    'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var search;
+
+function print( message ) {
+    document.write( '<p>' + message + '</p>');
 }
 
-function randomColor() {
-    var color = 'rgb(';
-    color += letsGetRandom() + ',';
-    color += letsGetRandom() + ',';
-    color += letsGetRandom() + ')';
-    return color;
+while (true) {
+    search = prompt("Search for a product. Type 'list' to show what is in stock, and type 'quit' to exit");
+    if (search === 'quit') {
+        break;
+    }
 }
-
-function print(message) {
-    document.write(message);
-}
-function ooohColors () {
-    html += '<div style="background-color:' + randomColor() + '"></div>';
-}
-
-for (i = 0; i < 10; i += 1) {
-    ooohColors();
-}
-
-print(html);
