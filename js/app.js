a7ebb5ea4b0f7370c5814e0b46039c2bdb7850c0
slerpy <slerpy@riseup.net>
2015-06-19 01:47:26 -0700

    on event functions.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-//Hide Warning
-//Show Warning Slowly
-$(".warning").hide().show("slow");
+//Prevent spoilerphobes from seeing spoilers
+//Solution: Hide spoilers and reveal them through user inte
+
+//Prevent spoilerphobes from seeing spoilers
+// 1, Hide spoiler
+$(".spoiler span").hide();
+// 2, Add a button
+$(".spoiler").append("<button>Show Spoiler!</button>");
+// 3, add button press event
+$("button").click(function(){
+    //3.1 show spoiler next to button clicked
+    $(this).prev().show();
+    $(".spoiler span").show();
+    //3.2 get rid of button
+    $(this).remove();
+
+});
+
+
+
+
+//Solution: Hide Spoilers
