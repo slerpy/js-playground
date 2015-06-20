958cb4e32238dc4e24ddd34fa2af96ac05bf256a
slerpy <slerpy@riseup.net>
2015-06-19 23:22:58 -0700

    The Plan!

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@
 
 
 
+// 1, do click event on an image link.
+  //1,1 show overlay
+  //1,2 display fullsize(?) image.
+  //1,3 use childs alt attr as a description/caption.
+
+// 2, add overlay
+  // 2,1 image
+  // 2,2 description
+
+//3, on overlay click
+  //3,1 hide the overlay
+
+
+
+
 
 
 
